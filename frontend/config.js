9122a01e23731313c0b117de7cc31300b2ccb01c
Prayag Gangwar <prayag.gangwar162@gmail.com>
2026-05-27 20:59:42 +0530

    lms update

--- frontend/config.js
+++ frontend/config.js
@@ -4,4 +4,6 @@
 const PRODUCTION_URL = "https://library-management-system-4l2w.onrender.com/api";
 const LOCAL_URL = "http://localhost:5000/api";
 
-const API_BASE_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") ? LOCAL_URL : PRODUCTION_URL;
+const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" || window.location.hostname === "";
+
+const API_BASE_URL = isLocal ? LOCAL_URL : PRODUCTION_URL;
